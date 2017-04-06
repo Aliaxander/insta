cc40222d7c911aec4dbce867198edf199d087e4a
Aliaxander <admin@oxcpa.ru>
2017-04-06 14:44:19 +0300

    fix

--- OxApp/console/likes.php
+++ OxApp/console/likes.php
@@ -89,7 +89,8 @@ class Likes extends Command
             $status = true;
             while ($status = true) {
                 $accRow = InstBase::limit([0 => 1])->find(['status' => 0]);
-                $acc = "{$accRow->rows[0]->account}";
+                $acc = preg_replace("/[^0-9]/", '', $accRow->rows[0]->account);
+                
                 InstBase::where(['id' => $accRow->rows[0]->id])->update(['status' => 1]);
                 
                 echo "Set acc $acc:\n";
