8b76cbd2288036eac34231bd59f2cdd9d7c50132
Aliaxander <admin@oxcpa.ru>
2017-04-07 13:07:21 +0300

    test upload photo

--- OxApp/console/EditProfile.php
+++ OxApp/console/EditProfile.php
@@ -55,31 +55,31 @@ class EditProfile extends Command
             $api->username = $user->userName;
             $api->login($user->guid, $user->phoneId, $user->deviceId, $user->password);
             
-            //            //SetPhoto:
-            //            $api->changeProfilePicture($photo);
-            //            unlink($photo);
-            //            sleep(rand(3, 10));
+                        //SetPhoto:
+                        $api->changeProfilePicture($photo);
+                        unlink($photo);
+                        sleep(rand(3, 10));
             
-            //            $profiles = ProfileGenerate::limit([0 => 1])->find(['status' => 0])->rows[0];
-            //            ProfileGenerate::where(['id' => $profiles->id])->update(['status' => 1]);
-            //            $word = [$user->userName, $user->firstName, mt_rand(10000, 99999)];
-            //            $word = $word[mt_rand(0, 2)];
-            //            $word = str_replace([' ', '.'], '', $word);
-            //            $biography = $profiles->description;
-            //            $url = mb_strtolower(str_replace('%username%', $word, $profiles->url));
-            //            $profile = $api->edit($biography, $url, $user->phoneId, $user->firstName,
-            //                $user->email);//dat1ng.cf/i-love-to-do-it
-            //            print_r($profile);
+                        $profiles = ProfileGenerate::limit([0 => 1])->find(['status' => 0])->rows[0];
+                        ProfileGenerate::where(['id' => $profiles->id])->update(['status' => 1]);
+                        $word = [$user->userName, $user->firstName, mt_rand(10000, 99999)];
+                        $word = $word[mt_rand(0, 2)];
+                        $word = str_replace([' ', '.'], '', $word);
+                        $biography = $profiles->description;
+                        $url = mb_strtolower(str_replace('%username%', $word, $profiles->url));
+                        $profile = $api->edit($biography, $url, $user->phoneId, $user->firstName,
+                            $user->email);//dat1ng.cf/i-love-to-do-it
+                        print_r($profile);
             
-            $dir = scandir('/home/photos2');
-            unset($dir[array_search('.', $dir)]);
-            unset($dir[array_search('..', $dir)]);
-            $dir = array_values($dir);
-            $photo = '/home/photos2/' . $dir[rand(0, count($dir) - 1)];
-            
-            $api->uploadPhoto($photo);
-            unlink($photo);
-            sleep(rand(3, 10));
+//            $dir = scandir('/home/photos2');
+//            unset($dir[array_search('.', $dir)]);
+//            unset($dir[array_search('..', $dir)]);
+//            $dir = array_values($dir);
+//            $photo = '/home/photos2/' . $dir[rand(0, count($dir) - 1)];
+//
+//            $api->uploadPhoto($photo);
+//            unlink($photo);
+//            sleep(rand(3, 10));
             
             Users::where(['id' => $user->id])->update([
                 'login' => 1,
