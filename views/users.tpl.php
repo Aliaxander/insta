e7a638767256a1956c88fbd41ee4a9d73e136d4c
kinky <bagirus112@gmail.com>
2017-04-17 22:40:31 +0300

    fix users tpl

--- views/users.tpl.php
+++ views/users.tpl.php
@@ -1,104 +1,113 @@
 {% include "global/head.tpl.php" %}
-<div class="table-responsive">
-    Total likes: {{sumLikes}}
-    <table class="table">
-        <thead>
-        <tr>
-            <form method="get">
-                <th></th>
-                <th></th>
-                <th><select class="form-control input-sm" name="userGroup">
-                        <option value="">all</option>
-                        {% for group in groups %}
-                        <option value="{{ group.id }}">{{ group.name }}</option>
-                        {% endfor %}
-                    </select></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th><select class="form-control input-sm" name="LogIn">
-                        <option value="">all</option>
-                        <option value="0">LogIn 0</option>
-                        <option value="1">LogIn 1</option>
-                    </select></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th>
-                    <select class="form-control input-sm" name="ban">
-                        <option value="">all</option>
-                        <option value="0">No ban</option>
-                        <option value="1">Ban</option>
-                    </select></th>
-                <th>
-                    <button type="submit" class="btn btn-primary">Filter</button>
-                </th>
-            </form>
-        </tr>
-        </thead>
-        <thead>
-        <tr>
-            <th>#</th>
-            <th><a href="?orderBy=id&sort=desc">id</a></th>
-            <th><a href="?orderBy=userGroup&sort=desc">userGroup</a></th>
-            <th><a href="?orderBy=userName&sort=desc">userName</a></th>
-            <th><a href="?orderBy=firstName&sort=desc">firstName</a></th>
-            <th><a href="?orderBy=email&sort=desc">email</a></th>
-            <th><a href="?orderBy=logIn&sort=desc">logIn</a></th>
-            <th><a href="?orderBy=proxy&sort=desc">proxy</a></th>
-            <th><a href="?orderBy=requests&sort=desc">requests</a></th>
-            <th><a href="?orderBy=follows&sort=desc">follows</a></th>
-            <th><a href="?orderBy=likes&sort=desc">likes</a></th>
-            <th><a href="?orderBy=dateCreate&sort=desc">dateCreate</a></th>
-            <th><a href="?orderBy=hour&sort=desc">hour</a></th>
-            <th><a href="?orderBy=ban&sort=desc">ban</a></th>
-            <th>options</th>
-        </tr>
-        </thead>
-        <tbody>
-        {% for user in users %}
-        {% if user.ban %}
-        <tr class="danger">
-            {% elseif user.ban == 0 and user.logIn == 1 %}
-        <tr class="success">
-            {% else %}
-        <tr>
-            {% endif %}
-            <td><input type="checkbox" value="{{ user.id }}"></td>
-            <td>{{ user.id }}</td>
-            <td>{{ user.userGroup }}</td>
-            <td><a href="https://instagram.com/{{ user.userName }}" target="_blank">{{ user.userName }}</a></td>
-            <td>{{ user.firstName }}</td>
-            <td>{{ user.email }}</td>
-            <td>{{ user.logIn }}</td>
-            <td>{{ user.proxy }}</td>
-            <td>{{ user.requests }}</td>
-            <td>{{ user.follows }}</td>
-            <td>{{ user.likes }}</td>
-            <td>{{ user.dateCreate }}</td>
-            <td>{{ user.hour }}</td>
-            <td>{{ user.ban }}</td>
-            <td><a href="/deleteUsers?id={{ user.id }}"><span class="glyphicon glyphicon-trash"></a></span></td>
-        </tr>
-        {% endfor %}
-        </tbody>
-    </table>
-    <button class="btn btn-danger">Delete</button>
-    <button class="btn btn-success" id="checkAll">check All</button>
-    <button class="btn btn-primary" id="addGroup" data-toggle="modal" onclick="check()"
-            data-target=".bs-example-modal-sm">Add to Group
-    </button>
-    <ul class="pagination pull-right" style="margin: 0;">
-        {% for i in range(1, totalPages) %}
-        <li
-                {% if i==setPage %} class="active" {% endif %}><a
-                    href="/users?page={{ i }}">{{ i }} {% if i==setPage %}<span class="sr-only">(current)</span>{% endif
-                %}</a></li>
-        {% endfor %}
-    </ul>
+<div class="panel panel-primary">
+    <div class="panel-heading">
+        <h3 class="panel-title">Users || Total likes: {{sumLikes}} || Total users: {{ totalRows }}</h3>
+    </div>
+    <div class="panel-body">
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                <tr>
+                    <form method="get">
+                        <th></th>
+                        <th></th>
+                        <th><select class="form-control" name="userGroup">
+                                <option value="">all</option>
+                                {% for group in groups %}
+                                <option value="{{ group.id }}">{{ group.name }}</option>
+                                {% endfor %}
+                            </select></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th><select class="form-control" name="LogIn">
+                                <option value="">all</option>
+                                <option value="0">LogIn 0</option>
+                                <option value="1">LogIn 1</option>
+                            </select></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th>
+                            <select class="form-control" name="ban">
+                                <option value="">all</option>
+                                <option value="0">No ban</option>
+                                <option value="1">Ban</option>
+                            </select></th>
+                        <th>
+                            <button type="submit" class="btn btn-primary">Filter</button>
+                        </th>
+                    </form>
+                </tr>
+                </thead>
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th><a href="?orderBy=id&sort=desc">id</a></th>
+                    <th><a href="?orderBy=userGroup&sort=desc">userGroup</a></th>
+                    <th><a href="?orderBy=userName&sort=desc">userName</a></th>
+                    <th><a href="?orderBy=firstName&sort=desc">firstName</a></th>
+                    <th><a href="?orderBy=email&sort=desc">email</a></th>
+                    <th><a href="?orderBy=logIn&sort=desc">logIn</a></th>
+                    <th><a href="?orderBy=proxy&sort=desc">proxy</a></th>
+                    <th><a href="?orderBy=requests&sort=desc">requests</a></th>
+                    <th><a href="?orderBy=follows&sort=desc">follows</a></th>
+                    <th><a href="?orderBy=likes&sort=desc">likes</a></th>
+                    <th><a href="?orderBy=dateCreate&sort=desc">dateCreate</a></th>
+                    <th><a href="?orderBy=hour&sort=desc">hour</a></th>
+                    <th><a href="?orderBy=ban&sort=desc">ban</a></th>
+                    <th>options</th>
+                </tr>
+                </thead>
+                <tbody>
+                {% for user in users %}
+                {% if user.ban %}
+                <tr class="danger">
+                    {% elseif user.ban == 0 and user.logIn == 1 %}
+                <tr class="success">
+                    {% else %}
+                <tr>
+                    {% endif %}
+                    <td><input type="checkbox" value="{{ user.id }}"></td>
+                    <td>{{ user.id }}</td>
+                    <td>{{ user.userGroup }}</td>
+                    <td><a href="https://instagram.com/{{ user.userName }}" target="_blank">{{ user.userName }}</a></td>
+                    <td>{{ user.firstName }}</td>
+                    <td>{{ user.email }}</td>
+                    <td>{{ user.logIn }}</td>
+                    <td>{{ user.proxy }}</td>
+                    <td>{{ user.requests }}</td>
+                    <td>{{ user.follows }}</td>
+                    <td>{{ user.likes }}</td>
+                    <td>{{ user.dateCreate }}</td>
+                    <td>{{ user.hour }}</td>
+                    <td>{{ user.ban }}</td>
+                    <td><a href="/deleteUsers?id={{ user.id }}"><span class="glyphicon glyphicon-trash"></a></span></td>
+                </tr>
+                {% endfor %}
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="panel-footer">
+        <button class="btn btn-danger">Delete</button>
+        <button class="btn btn-success" id="checkAll">check All</button>
+        <button class="btn btn-primary" id="addGroup" data-toggle="modal" onclick="check()"
+                data-target=".bs-example-modal-sm">Add to Group
+        </button>
+        <ul class="pagination pull-right" style="margin: 0;">
+            {% for i in range(1, totalPages) %}
+            <li
+                    {% if i==setPage %} class="active" {% endif %}><a
+                        href="/users?page={{ i }}">{{ i }} {% if i==setPage %}<span class="sr-only">(current)</span>{%
+                    endif
+                    %}</a></li>
+            {% endfor %}
+        </ul>
+    </div>
 </div>
 
 <div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
@@ -112,10 +121,10 @@
                 </div>
                 <div class="modal-body">
                     <div class="row">
-                        <select class="form-control input-sm" name="userGroup">
-                        {% for group in groups %}
-                        <option value="{{ group.id }}">{{ group.name }}</option>
-                        {% endfor %}
+                        <select class="form-control input-lg" name="userGroup">
+                            {% for group in groups %}
+                            <option value="{{ group.id }}">{{ group.name }}</option>
+                            {% endfor %}
                         </select>
                         <input type="hidden" class="form-control" id="id_to_group" name="id">
                     </div>
