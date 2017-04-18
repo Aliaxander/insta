7d006d0498565d88245a90724da78f2986e5538e
Aliaxander <admin@oxcpa.ru>
2017-04-18 20:18:19 +0300

    remove

--- OxApp/helpers/GoodDevices.php
+++ OxApp/helpers/GoodDevices.php
@@ -34,7 +34,6 @@ class GoodDevices
         '23/6.0.1; 480dpi; 1080x1920; ZTE; Z981; urd; qcom',
         '23/6.0.1; 640dpi; 1440x2560; ZTE; ZTE A2017U; ailsa_ii; qcom',
         '23/6.0.1; 640dpi; 1440x2392; LGE/lge; RS988; h1; h1',
-        '24/7.0; 480dpi; 1776x1080; motorola; Moto G (4); athene_f; qcom',
         '24/7.0; 380dpi; 1080x1920; OnePlus; ONEPLUS A3010; OnePlus3T; qcom',
     ];
     
