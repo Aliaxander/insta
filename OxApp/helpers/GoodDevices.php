9639811d78cb6b096f697e3cc2bed12da216fd0d
Aliaxander <admin@oxcpa.ru>
2017-04-18 19:27:59 +0300

    add new devices

--- OxApp/helpers/GoodDevices.php
+++ OxApp/helpers/GoodDevices.php
@@ -20,36 +20,26 @@ class GoodDevices
    * LAST-UPDATED: MARCH 2017.
    */
     const DEVICES = [
-        /* OnePlus 3T. Released: November 2016.
-         * https://www.amazon.com/OnePlus-A3010-64GB-Gunmetal-International/dp/B01N4H00V8
-         * https://www.handsetdetection.com/properties/devices/OnePlus/A3010
-         */
         '24/7.0; 380dpi; 1080x1920; OnePlus; ONEPLUS A3010; OnePlus3T; qcom',
-        /* LG G5. Released: April 2016.
-         * https://www.amazon.com/LG-Unlocked-Phone-Titan-Warranty/dp/B01DJE22C2
-         * https://www.handsetdetection.com/properties/devices/LG/RS988
-         */
         '23/6.0.1; 640dpi; 1440x2392; LGE/lge; RS988; h1; h1',
-        /* Huawei Mate 9 Pro. Released: January 2017.
-         * https://www.amazon.com/Huawei-Dual-Sim-Titanium-Unlocked-International/dp/B01N9O1L6N
-         * https://www.handsetdetection.com/properties/devices/Huawei/LON-L29
-         */
         '24/7.0; 640dpi; 1440x2560; HUAWEI; LON-L29; HWLON; hi3660',
-        /* ZTE Axon 7. Released: June 2016.
-         * https://www.frequencycheck.com/models/OMYDK/zte-axon-7-a2017u-dual-sim-lte-a-64gb
-         * https://www.handsetdetection.com/properties/devices/ZTE/A2017U
-         */
         '23/6.0.1; 640dpi; 1440x2560; ZTE; ZTE A2017U; ailsa_ii; qcom',
-        /* Samsung Galaxy S7 Edge SM-G935F. Released: March 2016.
-         * https://www.amazon.com/Samsung-SM-G935F-Factory-Unlocked-Smartphone/dp/B01C5OIINO
-         * https://www.handsetdetection.com/properties/devices/Samsung/SM-G935F
-         */
         '23/6.0.1; 640dpi; 1440x2560; samsung; SM-G935F; hero2lte; samsungexynos8890',
-        /* Samsung Galaxy S7 SM-G930F. Released: March 2016.
-         * https://www.amazon.com/Samsung-SM-G930F-Factory-Unlocked-Smartphone/dp/B01J6MS6BC
-         * https://www.handsetdetection.com/properties/devices/Samsung/SM-G930F
-         */
         '23/6.0.1; 640dpi; 1440x2560; samsung; SM-G930F; herolte; samsungexynos8890',
+        '23/6.0.1; 480dpi; 1080x1920; samsung; SM-C5000; c5ltechn; qcom;',
+        '23/6.0.1; 640dpi; 1440x2560; samsung; SM-G925F; zerolte; samsungexynos7420;',
+        '24/7.0; 480dpi; 1776x1080; motorola; Moto G (4); athene_f; qcom;',
+        '23/6.0.1; 640dpi; 1440x2560; samsung; SAMSUNG-SM-G935A; hero2qlteatt; qcom;',
+        '21/5.0.1; 480dpi; 1080x1920; samsung; GT-I9505; jflte; qcom;',
+        '23/6.0.1; 480dpi; 1080x1920; ZTE; Z981; urd; qcom;',
+        '24/7.0; 480dpi; 1080x1920; samsung; SM-G930K; heroltektt; samsungexynos8890;',
+        '23/6.0.1; 160dpi; 768x1024; samsung; SM-T550; gt510wifi; qcom;',
+        '22/5.1.1; 240dpi; 791x480; LGE/lge; LG-K120; me1; me1;',
+        '23/6.0.1; 480dpi; 1080x1920; samsung; SM-G900F; klte; qcom;',
+        '23/6.0.1; 320dpi; 720x1280; samsung; SM-J500F; j5lte; qcom;',
+        '23/6.0; 320dpi; 720x1208; HUAWEI/Huawei; H60-L04; hwH60; hi3630;',
+        '23/6.0.1; 320dpi; 720x1280; samsung; SM-J500M; j5lte; qcom;',
+        '21/5.0; 320dpi; 720x1184; HUAWEI/hi6210sft; hi6210sft; hi6210sft; hi6210sft;'
     ];
     
     /**
