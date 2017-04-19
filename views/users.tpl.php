a7edacacefef2df98cf0bdc5d7c4f9b4caba20f3
Aliaxander <admin@oxcpa.ru>
2017-04-20 01:25:31 +0300

    pagination

--- views/users.tpl.php
+++ views/users.tpl.php
@@ -38,7 +38,7 @@
                data-pagination="true"
                data-filter-control="true"
                data-click-to-select="true"
-               data-page-list="[5, 10, 20, 50, 100, 200, 500, 1000, 5000, ALL]">
+               data-page-list="[25, 50, 100, 200, 500, 1000, 5000, ALL]">
             <thead>
             <tr>
                 <th data-field="state" data-checkbox="true"></th>
