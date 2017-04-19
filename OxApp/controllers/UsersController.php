c03c886bb54eb16f67881e36895e27267af2b7df
kinky <bagirus112@gmail.com>
2017-04-19 21:25:27 +0300

    fix controller

--- OxApp/controllers/UsersController.php
+++ OxApp/controllers/UsersController.php
@@ -26,78 +26,11 @@ class UsersController extends App
      */
     public function get()
     {
-        $rule = [
-            'LogIn',
-            'ban',
-            'userGroup',
-            'userTask'
-        ];
-        $where = [];
-        foreach ($rule as $item) {
-            if (!empty($this->request->query->get($item))) {
-                $where[$item] = $this->request->query->get($item);
-            }
-        }
-        
-        if (!empty($this->request->query->get('orderBy')) && !empty($this->request->query->get('sort'))) {
-            $orderBy = [$this->request->query->get('orderBy') => $this->request->query->get('sort')];
-        } else {
-            $orderBy = ['id' => 'desc'];
-        }
-        if (!empty($this->request->query->get('limit'))) {
-            $limit = $this->request->query->get('limit');
-        } else {
-            $limit = 50;
-        }
-        if (!empty($this->request->query->get("page"))) {
-            $page = $this->request->query->get("page");
-        } else {
-            $page = 1;
-        }
-        $startPage = $page * $limit - $limit;
-        $paging = array($startPage => $limit);
-        $total = Users::selectBy("count(id) as count")
-            ->where($where)
-            ->orderBy(["id" => "desc"])
-            ->find();
-        $users = Users::orderBy($orderBy)
-            ->where($where)
-            ->limit($paging)
-            ->find()
-            ->rows;
-        if ($this->request->query->get('detail') == 'all') {
-            $template = 'userDetail';
-        } else {
-            $template = 'users';
-        }
         $taskType = TaskType::find()->rows;
-        foreach ($taskType as $item) {
-            $taskTypes[$item->id] = $item->name;
-        }
         $group = UserGroup::find()->rows;
-        foreach ($group as $item) {
-            $groups[$item->id] = $item->name;
-        }
-        foreach ($users as $key => $user) {
-            $users[$key]->userGroup = $groups[$user->userGroup];
-            $users[$key]->userTask = $taskTypes[$user->userTask];
-        }
-        $usersSum = @Users::selectBy(['sum(likes) as likes'])->find()->rows[0]->likes;
-        if ($_SERVER['REQUEST_URI'] == "/users") {
-            $url = $_SERVER['REQUEST_URI'] . "?";
-        } else {
-            $url = $_SERVER['REQUEST_URI'] . "&";
-        }
-        
-        return View::build($template, [
-            'url' => $url,
+        return View::build('users', [
             'taskTypes' => $taskType,
-            'groups' => $group,
-            'users' => $users,
-            'setPage' => $page,
-            'sumLikes' => $usersSum,
-            'totalRows' => (int)@$total->rows[0]->count,
-            'totalPages' => ceil(@$total->rows[0]->count / $limit),
+            'groups' => $group
         ]);
     }
     
