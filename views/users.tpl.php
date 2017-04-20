406b9cd7e3e7e7fdeb686fcaab7f10d85b9cbe51
kinky <bagirus112@gmail.com>
2017-04-20 11:31:05 +0300

    fix visible false date create

--- views/users.tpl.php
+++ views/users.tpl.php
@@ -77,7 +77,7 @@
                 <th data-field="day" data-visible="false" data-sortable="true">day</th>
                 <th data-field="hour" data-sortable="true">hour</th>
                 <th data-field="month" data-visible="false" data-sortable="true">month</th>
-                <th data-field="dateCreate" data-sortable="true">dateCreate</th>
+                <th data-field="dateCreate" data-visible="false" data-sortable="true">dateCreate</th>
                 <th data-field="dateUpdate" data-sortable="true">dateUpdate</th>
                 <th data-field="ban" data-filter-control="select" data-filter-data="var:ban" data-sortable="true">ban
                 </th>
