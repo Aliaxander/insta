fa5290c8307823a9a090aa847d4cfe1dc813a7f9
kinky <bagirus112@gmail.com>
2017-04-20 18:55:50 +0300

    add footer table

--- views/users.tpl.php
+++ views/users.tpl.php
@@ -33,6 +33,7 @@
                data-page-list="[50, 100, 200, 500, 1000, 5000]"
                data-url="/api/users"
                data-show-columns="true"
+               data-show-footer="true"
                data-search="true"
                data-cookie="true"
                data-cookie-id-table="saveId"
@@ -50,13 +51,13 @@
             <tr>
                 <th data-field="state" data-checkbox="true"></th>
                 <th data-field="id" data-sortable="true">ID</th>
-                <th data-field="userGroup" data-filter-control="select" data-filter-data="var:userGroup"
+                <th data-field="userGroup" data-filter-control="select" data-align="center" data-filter-data="var:userGroup"
                     data-sortable="true">userGroup
                 </th>
-                <th data-field="userTask" data-filter-control="select" data-filter-data="var:userTask"
+                <th data-field="userTask" data-filter-control="select" data-align="center" data-filter-data="var:userTask"
                     data-sortable="true">userTask
                 </th>
-                <th data-field="userName" data-sortable="true" data-formatter="userNameFormatter">userName</th>
+                <th data-field="userName" data-sortable="true" data-align="center" data-formatter="userNameFormatter">userName</th>
                 <th data-field="firstName" data-visible="false" data-sortable="true">firstName</th>
                 <th data-field="email" data-visible="false" data-sortable="true">email</th>
                 <th data-field="password" data-visible="false" data-sortable="true">password</th>
@@ -65,7 +66,7 @@
                 <th data-field="waterfall_id" data-visible="false" data-sortable="true">waterfall_id</th>
                 <th data-field="guid" data-visible="false" data-sortable="true">guid</th>
                 <th data-field="qeId" data-visible="false" data-sortable="true">qeId</th>
-                <th data-field="logIn" data-filter-control="select" data-filter-data="var:logIn" data-sortable="true">
+                <th data-field="logIn" data-filter-control="select" data-align="center" data-filter-data="var:logIn" data-footer-formatter="footerFormatter" data-sortable="true">
                     logIn
                 </th>
                 <th data-field="csrftoken" data-visible="false" data-sortable="true">csrftoken</th>
@@ -76,15 +77,15 @@
                 <th data-field="url" data-visible="false" data-sortable="true">url</th>
                 <th data-field="proxy" data-sortable="true">proxy</th>
                 <th data-field="userAgent" data-visible="false" data-sortable="true">userAgent</th>
-                <th data-field="requests" data-sortable="true">requests</th>
-                <th data-field="follows" data-visible="false" data-sortable="true">follows</th>
-                <th data-field="likes" data-sortable="true">likes</th>
+                <th data-field="requests" data-sortable="true" data-align="center" data-footer-formatter="footerFormatter">requests</th>
+                <th data-field="follows" data-visible="false" data-sortable="true" data-align="center" data-footer-formatter="footerFormatter">follows</th>
+                <th data-field="likes" data-sortable="true" data-align="center" data-footer-formatter="footerFormatter">likes</th>
                 <th data-field="day" data-visible="false" data-sortable="true">day</th>
-                <th data-field="hour" data-sortable="true">hour</th>
+                <th data-field="hour" data-align="center" data-sortable="true">hour</th>
                 <th data-field="month" data-visible="false" data-sortable="true">month</th>
                 <th data-field="dateCreate" data-visible="false" data-sortable="true">dateCreate</th>
                 <th data-field="dateUpdate" data-sortable="true">dateUpdate</th>
-                <th data-field="ban" data-filter-control="select" data-filter-data="var:ban" data-sortable="true">ban
+                <th data-field="ban" data-filter-control="select" data-filter-data="var:ban" data-footer-formatter="footerFormatter" data-sortable="true">ban
                 </th>
             </tr>
             </thead>
@@ -271,6 +272,7 @@
         }
         return {};
     }
+
     function getHeight() {
         return $(window).height() - $('h1').outerHeight(true);
     }
@@ -285,6 +287,13 @@
         });
         html.push('</div>');
         return html.join('');
+    }
+    function footerFormatter(data) {
+        var field = this.field;
+        var total_sum = data.reduce(function(sum, row) {
+            return (sum) + (row[field] || 0);
+        }, 0);
+        return total_sum;
     }
 </script>
 {% include "global/footer.tpl.php" %}
