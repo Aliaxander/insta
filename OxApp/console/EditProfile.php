30b555dd22c0c155f584eeae8a8f0cd61e767862
Aliaxander <admin@oxcpa.ru>
2017-04-21 12:19:53 +0300

    unlim domain test dot com

--- OxApp/console/EditProfile.php
+++ OxApp/console/EditProfile.php
@@ -8,6 +8,7 @@
 
 namespace Acme\Console\Command;
 
+use Faker\Factory;
 use OxApp\helpers\FreenomReg;
 use OxApp\helpers\IgApi;
 use OxApp\models\Domains;
@@ -57,7 +58,6 @@ class EditProfile extends Command
                 unset($dir[array_search('.', $dir)]);
                 unset($dir[array_search('..', $dir)]);
                 $dir = array_values($dir);
-                
                 
                 $api = new IgApi();
                 $api->proxy = $user->proxy;
@@ -112,52 +112,63 @@ class EditProfile extends Command
                 //                if (rand(0, 1) == 1) {
                 //                    $domain = "http://" . $domain;
                 //                }
-                $domain = Domains::limit([0 => 1])->find(['status' => 0]);
-                if ($domain->count == 1) {
-                    Domains::where(['id' => $domain->rows[0]->id])->update(['status' => 1]);
-                    $profileResult = '';
-                    $i = 0;
-                    while ($profileResult === '') {
-                        //$biography
-                        $domain = $domain->rows[0]->domain;
-                        //                    $domains = ['.myblogonline.pw', '.blogonline.pw'];
-                        //                    $subDomain = [$user->firstName, $user->userName];
-                        //                    $domain = $subDomain[rand(0, 1)] . $domains[rand(0, 1)];
-                        //                    $domain = str_replace(" ", "", $domain);
-                        //                    $domain = mb_strtolower($domain);
-                        $profile = $api->edit($biography, $domain, $user->phoneId, $user->firstName,
-                            $user->email);
-                        $profileResult = $profile[1];
-                        if (empty($profile[1])) {
-                            $profileResult = '';
-                        }
-                        if ($i === 5) {
-                            $profileResult = false;
-                        }
-                        $i++;
+                
+                $faker = Factory::create();
+                //                $domain = Domains::limit([0 => 1])->find(['status' => 0]);
+                //                if ($domain->count == 1) {
+                // Domains::where(['id' => $domain->rows[0]->id])->update(['status' => 1]);
+                $profileResult = '';
+                $i = 0;
+                while ($profileResult === '') {
+                    //$biography
+                    //$domain = $domain->rows[0]->domain;
+                    // $domains = ['.myblogonline.pw', '.blogonline.pw'];
+                    $domain = $faker->userName . rand(1950, 2017) . '.love2live2.com ';
+                    $domain = str_replace(" ", "", $domain);
+                    $domain = str_replace("'", "", $domain);
+                    $domain = mb_strtolower($domain);
+                    $result = FreenomReg::freedomReg($domain);
+                    $p = xml_parser_create();
+                    xml_parse_into_struct($p, $result[1], $vals, $index);
+                    xml_parser_free($p);
+                    $domain = mb_strtolower($vals[2]['value']);
+                    if (rand(0, 1) == 1) {
+                        $domain = "http://" . $domain;
                     }
                     
-                    print_r($profile);
-                    
-                    //            $dir = scandir('/home/photos2');
-                    //            unset($dir[array_search('.', $dir)]);
-                    //            unset($dir[array_search('..', $dir)]);
-                    //            $dir = array_values($dir);
-                    //            $photo = '/home/photos2/' . $dir[rand(0, count($dir) - 1)];
-                    //
-                    //            $api->uploadPhoto($photo);
-                    //            unlink($photo);
-                    //            sleep(rand(3, 10));
-                    
-                    Users::where(['id' => $user->id])->update([
-                        'login' => 1,
-                        'biography' => $biography,
-                        'url' => $domain,
-                        'photo' => $profile[1]['user']['profile_pic_url']
-                    ]);
-                } else {
-                    die('no domains');
+                    $profile = $api->edit($biography, $domain, $user->phoneId, $user->firstName,
+                        $user->email);
+                    $profileResult = $profile[1];
+                    if (empty($profile[1])) {
+                        $profileResult = '';
+                    }
+                    if ($i === 5) {
+                        $profileResult = false;
+                    }
+                    $i++;
                 }
+                
+                print_r($profile);
+                
+                //            $dir = scandir('/home/photos2');
+                //            unset($dir[array_search('.', $dir)]);
+                //            unset($dir[array_search('..', $dir)]);
+                //            $dir = array_values($dir);
+                //            $photo = '/home/photos2/' . $dir[rand(0, count($dir) - 1)];
+                //
+                //            $api->uploadPhoto($photo);
+                //            unlink($photo);
+                //            sleep(rand(3, 10));
+                
+                Users::where(['id' => $user->id])->update([
+                    'login' => 1,
+                    'biography' => $biography,
+                    'url' => $domain,
+                    'photo' => $profile[1]['user']['profile_pic_url']
+                ]);
+                //                } else {
+                //                    die('no domains');
+                //                }
             } else {
                 die('no tasks');
             }
