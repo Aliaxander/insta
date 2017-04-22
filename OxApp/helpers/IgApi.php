80eb614f797856752bc685f8e39916f78c79f284
Aliaxander <admin@oxcpa.ru>
2017-04-22 18:42:52 +0300

    fix

--- OxApp/helpers/IgApi.php
+++ OxApp/helpers/IgApi.php
@@ -192,7 +192,7 @@ class IgApi
                 $data=[];
                 $data['phone_number'] = '79356658544';
                 $data['csrfmiddlewaretoken'] = $token;
-                print_r($checkPoint->request('https://www.instagram.com/challenge/', null, $data));
+                print_r($checkPoint->request('https://i.instagram.com/challenge/', null, $data));
             }
             //
             //                        print_r($checkPoint->request('https://i.instagram.com/challenge/?next=instagram://checkpoint/dismiss'));
