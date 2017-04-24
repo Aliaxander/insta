71fda6244398d31552636e54314af0aa26fa3b1b
Aliaxander <admin@oxcpa.ru>
2017-04-24 23:38:04 +0300

    fix

--- OxApp/console/EditProfile.php
+++ OxApp/console/EditProfile.php
@@ -81,124 +81,129 @@ class EditProfile extends Command
                         die('Wait limit subnet ip');
                     }
                 }
-                Users::where(['id' => $user->id])->update(['userTask' => 3]);
-                $dir = scandir('/home/photos');
-                unset($dir[array_search('.', $dir)]);
-                unset($dir[array_search('..', $dir)]);
-                $dir = array_values($dir);
-                
-                $api = new IgApi();
-                $api->proxy = $user->proxy;
-                $api->username = $user->userName;
-                $loginResult = '';
-                $i = 0;
-                while ($loginResult === '') {
-                    $login = $api->login($user->guid, $user->phoneId, $user->deviceId, $user->password);
-                    $loginResult = $login[1];
-                    if ($i === 5) {
-                        $loginResult = false;
-                    }
-                    $i++;
-                }
-                
-                //SetPhoto:
-                $photoResult = '';
-                $result = true;
-                $i = 0;
-                while ($photoResult === '') {
-                    $photo = '/home/photos/' . $dir[rand(0, count($dir) - 1)];
-                    $result = $api->changeProfilePicture($photo);
-                    $photoResult = $result[1];
-                    if ($i === 5) {
-                        $photoResult = false;
-                    }
-                    $i++;
-                }
-                if (@$result[1]['message'] === 'checkpoint_required' && !empty($user->id)) {
-                    Users::where([
-                        'id' => $user->id
-                    ])->update(['ban' => 1]);
-                    die("Account banned");
-                }
-                
-                //unlink($photo);
-                
-                $profiles = ProfileGenerate::limit([0 => 1])->find(['status' => 0])->rows[0];//groupBy('description')->
-                ProfileGenerate::where(['id' => $profiles->id])->update(['status' => 1]);
-                //            $word = [$user->userName, $user->firstName, mt_rand(10000, 99999)];
-                //            $word = $word[mt_rand(0, 2)];
-                //            $word = str_replace([' ', '.'], '', $word);
-                $biography = @$profiles->description;
-                //            $url = mb_strtolower(str_replace('%username%', $word, $profiles->url));
-                //            //
-                //            //
-                //                $result = FreenomReg::freedomReg($profiles->url);
-                //                $p = xml_parser_create();
-                //                xml_parse_into_struct($p, $result[1], $vals, $index);
-                //                xml_parser_free($p);
-                //                $domain = mb_strtolower($vals[2]['value']);
-                //                if (rand(0, 1) == 1) {
-                //                    $domain = "http://" . $domain;
-                //                }
-                
-                //$faker = Factory::create();
-                $domain = Domains::limit([0 => 1])->find(['status' => 0]);
-                if ($domain->count == 1) {
-                    Domains::where(['id' => $domain->rows[0]->id])->update(['status' => 1]);
-                    $domain = $domain->rows[0]->domain;
-                    if (rand(0, 1) == 1) {
-                        $domain = "http://" . $domain;
-                    }
-                    $domain = str_replace(" ", "", $domain);
-                    $profileResult = '';
+                if (!empty($user)) {
+                    Users::where(['id' => $user->id])->update(['userTask' => 3]);
+                    $dir = scandir('/home/photos');
+                    unset($dir[array_search('.', $dir)]);
+                    unset($dir[array_search('..', $dir)]);
+                    $dir = array_values($dir);
+                    
+                    $api = new IgApi();
+                    $api->proxy = $user->proxy;
+                    $api->username = $user->userName;
+                    $loginResult = '';
                     $i = 0;
-                    while ($profileResult === '') {
-                        //$biography
-                        //$domain = $domain->rows[0]->domain;
-                        // $domains = ['.myblogonline.pw', '.blogonline.pw'];
-                        //$domain = $faker->userName . rand(1950, 2017) . '.love2live2.com ';
-                        //                    $domain = str_replace(" ", "", $domain);
-                        //                    $domain = str_replace("'", "", $domain);
-                        //                    $domain = mb_strtolower($domain);
-                        //                    $result = FreenomReg::freedomReg($domain);
-                        //                    $p = xml_parser_create();
-                        //                    xml_parse_into_struct($p, $result[1], $vals, $index);
-                        //                    xml_parser_free($p);
-                        //                    $domain = mb_strtolower($vals[2]['value']);
-                        //                    if (rand(0, 1) == 1) {
-                        //                        $domain = "http://" . $domain;
-                        //                    }
-                        
-                        $profile = $api->edit($biography, $domain, $user->phoneId, $user->firstName,
-                            $user->email);
-                        $profileResult = $profile[1];
-                        if (empty($profile[1])) {
-                            $profileResult = '';
-                        }
+                    while ($loginResult === '') {
+                        $login = $api->login($user->guid, $user->phoneId, $user->deviceId, $user->password);
+                        $loginResult = $login[1];
                         if ($i === 5) {
-                            $profileResult = false;
+                            $loginResult = false;
                         }
                         $i++;
                     }
                     
-                    print_r($profile);
+                    //SetPhoto:
+                    $photoResult = '';
+                    $result = true;
+                    $i = 0;
+                    while ($photoResult === '') {
+                        $photo = '/home/photos/' . $dir[rand(0, count($dir) - 1)];
+                        $result = $api->changeProfilePicture($photo);
+                        $photoResult = $result[1];
+                        if ($i === 5) {
+                            $photoResult = false;
+                        }
+                        $i++;
+                    }
+                    if (@$result[1]['message'] === 'checkpoint_required' && !empty($user->id)) {
+                        Users::where([
+                            'id' => $user->id
+                        ])->update(['ban' => 1]);
+                        die("Account banned");
+                    }
                     
-                    //            $dir = scandir('/home/photos2');
-                    //            unset($dir[array_search('.', $dir)]);
-                    //            unset($dir[array_search('..', $dir)]);
-                    //            $dir = array_values($dir);
-                    //            $photo = '/home/photos2/' . $dir[rand(0, count($dir) - 1)];
-                    //
-                    //            $api->uploadPhoto($photo);
-                    //            unlink($photo);
-                    //            sleep(rand(3, 10));
+                    //unlink($photo);
                     
-                    Users::where(['id' => $user->id])->update([
-                        'login' => 1,
-                        'biography' => $biography,
-                        'url' => $domain,
-                        'photo' => $profile[1]['user']['profile_pic_url']
-                    ]);
+                    $profiles = ProfileGenerate::limit([0 => 1])->find(['status' => 0])->rows[0];//groupBy('description')->
+                    ProfileGenerate::where(['id' => $profiles->id])->update(['status' => 1]);
+                    //            $word = [$user->userName, $user->firstName, mt_rand(10000, 99999)];
+                    //            $word = $word[mt_rand(0, 2)];
+                    //            $word = str_replace([' ', '.'], '', $word);
+                    $biography = @$profiles->description;
+                    //            $url = mb_strtolower(str_replace('%username%', $word, $profiles->url));
+                    //            //
+                    //            //
+                    //                $result = FreenomReg::freedomReg($profiles->url);
+                    //                $p = xml_parser_create();
+                    //                xml_parse_into_struct($p, $result[1], $vals, $index);
+                    //                xml_parser_free($p);
+                    //                $domain = mb_strtolower($vals[2]['value']);
+                    //                if (rand(0, 1) == 1) {
+                    //                    $domain = "http://" . $domain;
+                    //                }
+                    
+                    //$faker = Factory::create();
+                    $domain = Domains::limit([0 => 1])->find(['status' => 0]);
+                    if ($domain->count == 1) {
+                        Domains::where(['id' => $domain->rows[0]->id])->update(['status' => 1]);
+                        $domain = $domain->rows[0]->domain;
+                        if (rand(0, 1) == 1) {
+                            $domain = "http://" . $domain;
+                        }
+                        $domain = str_replace(" ", "", $domain);
+                        $profileResult = '';
+                        $i = 0;
+                        while ($profileResult === '') {
+                            //$biography
+                            //$domain = $domain->rows[0]->domain;
+                            // $domains = ['.myblogonline.pw', '.blogonline.pw'];
+                            //$domain = $faker->userName . rand(1950, 2017) . '.love2live2.com ';
+                            //                    $domain = str_replace(" ", "", $domain);
+                            //                    $domain = str_replace("'", "", $domain);
+                            //                    $domain = mb_strtolower($domain);
+                            //                    $result = FreenomReg::freedomReg($domain);
+                            //                    $p = xml_parser_create();
+                            //                    xml_parse_into_struct($p, $result[1], $vals, $index);
+                            //                    xml_parser_free($p);
+                            //                    $domain = mb_strtolower($vals[2]['value']);
+                            //                    if (rand(0, 1) == 1) {
+                            //                        $domain = "http://" . $domain;
+                            //                    }
+                            
+                            $profile = $api->edit($biography, $domain, $user->phoneId, $user->firstName,
+                                $user->email);
+                            $profileResult = $profile[1];
+                            if (empty($profile[1])) {
+                                $profileResult = '';
+                            }
+                            if ($i === 5) {
+                                $profileResult = false;
+                            }
+                            $i++;
+                        }
+                        
+                        print_r($profile);
+                        
+                        //            $dir = scandir('/home/photos2');
+                        //            unset($dir[array_search('.', $dir)]);
+                        //            unset($dir[array_search('..', $dir)]);
+                        //            $dir = array_values($dir);
+                        //            $photo = '/home/photos2/' . $dir[rand(0, count($dir) - 1)];
+                        //
+                        //            $api->uploadPhoto($photo);
+                        //            unlink($photo);
+                        //            sleep(rand(3, 10));
+                        if (!empty($user->id)) {
+                            Users::where(['id' => $user->id])->update([
+                                'login' => 1,
+                                'biography' => $biography,
+                                'url' => $domain,
+                                'photo' => $profile[1]['user']['profile_pic_url']
+                            ]);
+                        }
+                    } else {
+                        die('no tasks');
+                    }
                 } else {
                     die('no domains');
                 }
