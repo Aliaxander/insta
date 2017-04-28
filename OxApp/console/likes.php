387a52c51551de8bc59e15b26abe5f8ce1d648f7
Aliaxander <admin@oxcpa.ru>
2017-04-28 14:26:30 +0300

    Fix's

--- OxApp/console/likes.php
+++ OxApp/console/likes.php
@@ -101,36 +101,37 @@ class Likes extends Command
                 Users::where(['id' => $user->id])->update(['csrftoken' => $tokenResult]);
             }
             //$api->login($user->guid, $user->phoneId, $user->deviceId, $user->password);
-
-             //Follow my accouns:
-                        $usersFollow = Users::orderBy(["id" => 'desc'])->find([
-                            'login/in' => [1, 2],
-                            'ban' => 0,
-                            'userTask' => 3,
-                            'accountId/>' => 0,
-                            'id/!=' => $user->id,
-                        ]);
-                        if ($usersFollow->count > 0) {
-                            $array = $usersFollow->rows;
-                            if ($usersFollow->count < 8) {
-                                $count = $usersFollow->count;
-                            } else {
-                                $count = 8;
-                            }
-                            $randUsers = mt_rand(1, $count);
-                            for ($i = 0; $i < $randUsers; $i++) {
-                                $rand = mt_rand(0, count($array));
-                                $randUser = $array[$rand];
-                                sleep(rand(1, 3));
-                                print_r($api->follow($randUser->accountId));
-                                $followCou++;
-                                $requestCou += 2;
-                                unset($array[$rand]);
-                                sleep(rand(10, 20));
-                            }
-                        }
-
-
+            
+            //Follow my accouns:
+            $usersFollow = Users::orderBy(["id" => 'desc'])->find([
+                'login/in' => [1, 2],
+                'ban' => 0,
+                'userTask' => 3,
+                'accountId/>' => 0,
+                'id/!=' => $user->id,
+            ]);
+            if ($usersFollow->count > 0) {
+                $array = $usersFollow->rows;
+                if ($usersFollow->count < 8) {
+                    $count = $usersFollow->count;
+                } else {
+                    $count = 8;
+                }
+                $randUsers = mt_rand(1, $count);
+                for ($i = 0; $i < $randUsers; $i++) {
+                    $rand = mt_rand(0, count($array));
+                    $randUser = $array[$rand];
+                    sleep(rand(1, 3));
+                    $result = $api->getFeed($randUser->accountId);
+                    print_r($result);
+                    print_r($api->follow($randUser->accountId));
+                    $followCou++;
+                    $requestCou += 2;
+                    unset($array[$rand]);
+                    sleep(rand(10, 20));
+                }
+            }
+            
             $status = true;
             while ($status = true) {
                 $userTest = Users::find(['id' => $user->id, 'ban' => 0]);
