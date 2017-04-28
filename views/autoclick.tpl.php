90bc5933f8b657c2362c4c4dd845769e8675ae0b
Aliaxander <admin@oxcpa.ru>
2017-04-28 23:58:14 +0300

    fix

--- views/autoclick.tpl.php
+++ views/autoclick.tpl.php
@@ -8,17 +8,5 @@
     });
 </script>
 {% if sess %}
-<script>
-    $(document).ready(function () {
-	    $.ajax({
-	        url: "/public/{{ sess }}.html", //Your url both relative and fixed path will work
-	        type: "GET", // you need post not get because you are sending a lot of da
-	        success: function (response) {
-	            document.write(response);
-	        }
-	    });
-    });
-</script>
-<div id="result"></div>
-<!--<iframe width="1000" height="1000" src="http://insta.oxgroup.media/public/{{ sess }}.html"></iframe>-->
+<iframe width="1000" height="1000" src="http://insta.oxgroup.media/public/{{ sess }}.html"></iframe>
 {% endif %}
