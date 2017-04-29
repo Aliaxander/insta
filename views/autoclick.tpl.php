cb16a67c8c93bc2b4df33a2f15b6f170dbbfb4fb
Aliaxander <admin@oxcpa.ru>
2017-04-29 15:16:12 +0300

    fix's

--- views/autoclick.tpl.php
+++ views/autoclick.tpl.php
@@ -1,6 +1,8 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 {% if sess %}
-{{ data }}
+        {% autoescape false %}
+            {{ data }}
+        {% autoescape %}
 {% endif %}
 <script>
     $(document).ready(function () {
