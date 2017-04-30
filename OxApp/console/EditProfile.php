311394f3fb48c9432ef3d9d0f59d7665347ea68d
Aliaxander <admin@oxcpa.ru>
2017-05-01 01:55:48 +0300

    new edit and likes

--- OxApp/console/EditProfile.php
+++ OxApp/console/EditProfile.php
@@ -174,7 +174,7 @@ class EditProfile extends Command
                             //                        $domain = "http://" . $domain;
                             //                    }
                             
-                            $profile = $api->oldEdit($biography, $domain, $user->phoneId, $user->firstName,
+                            $profile = $api->edit($biography, $domain, $user->phoneId, $user->firstName,
                                 $user->email);
                             $profileResult = $profile[1];
                             if (empty($profile[1])) {
