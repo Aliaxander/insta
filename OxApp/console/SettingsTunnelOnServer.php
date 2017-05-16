de4ffb3672d945814e2ec2d8c811f7329a9cac6d
Aliaxander <admin@oxcpa.ru>
2017-05-16 19:12:32 +0300

    + 48 ip

--- OxApp/console/SettingsTunnelOnServer.php
+++ OxApp/console/SettingsTunnelOnServer.php
@@ -109,7 +109,7 @@ class SettingsTunnelOnServer extends Command
                     'status' => 4,
                 ]);
                 Proxy::delete(['proxy/like' => $tunnel->serverIp . ':%']);
-                for ($i = 30000; $i < 30200; $i++) {
+                for ($i = 30000; $i < 30248; $i++) {
                     $proxy[] = Proxy::add([
                         'proxy' => $tunnel->serverIp . ":" . $i . ";",
                         'rand' => rand(0, 1000)
